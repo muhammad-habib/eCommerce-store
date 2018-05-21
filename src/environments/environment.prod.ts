245f47ae4a5b160a0c3bb14bd85d7355d107f9ff
Muhammad Fawzy Habib <muhammad.habib.dev@gmail.com>
2018-05-21 15:26:00 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT:'http://admin.zadfresh.com/api/',
+  API_ENDPOINT:'https://admin.zadfresh.com/api/',
   // API_ENDPOINT:'http://zad.app/api/',
 
 
