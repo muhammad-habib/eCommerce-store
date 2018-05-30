0dad682e06e0047f612560c864b5dff3ac05b53d
kazafy <kazafy@zadfresh.com>
2018-05-30 14:48:31 +0200

    landing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,7 @@ export const routes: Routes = [
     {
         path:'landing',component : LandingPageComponent
     },
+    {path:'',redirectTo: '/landing',pathMatch:'full'},
     { 
         path: '', 
         component: PagesComponent, children: [
