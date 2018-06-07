e64b1def2aba451360baee404582aeead2f0aa9a
Muhammad Fawzy Habib <muhammad.habib.dev@gmail.com>
2018-06-07 15:19:03 +0200

    fix

--- src/i18n/ar.ts
+++ src/i18n/ar.ts
@@ -11,6 +11,7 @@ export const LANG_AR_TRANS = {
     'help': 'مساعدة',
     'free_credit': 'رصيد مجانى',
     'my_wallet': 'محفظتى',
+    'my_points': 'نقاطى',
     // categories
     'all': 'الكل',
     // account menu
@@ -28,5 +29,50 @@ export const LANG_AR_TRANS = {
     'en' : 'English',
     'ar' : 'عربى',
     // search
-    'search':'أكتب لتبدأ البحث ...'
+    'search':'أكتب لتبدأ البحث ...',
+    // order
+    'delivery_info': 'معلومات التوصيل',
+    'products': 'المنتجات',
+    'unavailable_products': 'المنتجات الغير متاحة',
+    'payment_details': 'تفاصيل الدفع',
+    'order_received': 'استلام الطلب',
+    'order_received_msg': 'تم تاكيد الطلب وسيتم تجهيزه فى الوقت المحدد',
+    'prepare_order': 'تجهيز الطلب',
+    'prepare_order_msg': 'جارى تجهيز الطلب الان',
+    'on_way': 'الطلب فى الطريق',
+    'on_way_msg': 'طلبك تم تجهيزه وهو فى طريقه اليك',
+    'order_delivered': 'وصل طلبك',
+    'order_delivered_msg': 'وصل طلبك استمتع  بوقتك',
+    'delivery_time': 'وقت التوصيل',
+    'delivery_location': 'مكان التوصيل',
+    'order_value': 'قيمة الطلب',
+    'delivery_fees': 'قيمة التوصيل',
+    'vat_value': 'نسبة الضريبة المضافة',
+    'vat_rate': 'قيمة الضريبة المضافة',
+    'discount_value': 'قيمة الخصم',
+    'total_price': 'المجموع الكلى',
+    'SAR': 'ريال',
+    // product
+    'category': 'القسم',
+    'size': 'الحجم',
+    // My account
+    'my_account' : 'حسابى',
+    'name' : 'الاسم',
+    'email' : 'البريد الالكترونى',
+    // My Wallet
+    'wallet_balance' : 'الرصيد',
+    'active' : 'نشط',
+    'used' : 'مستخدم',
+    'expired' : 'منتهى الصلاحية',
+    // sign in
+    'country_key' : 'مفتاح الدولة',
+    'mobile' : 'الجوال',
+    'full_name' : 'الاسم كامل',
+    'social_sign_in' : 'أو تسجيل دخول باستخدام',
+    'no_account' : 'لا تمتلك حساب ؟  قم بانشاء حساب الان!',
+    // sms dialog
+    'sms_confirm': 'تاكيد رقم الجوال',
+    'code_number': 'أدخل الرمز الذى استلمته',
+    'code': 'الرمز'
+
 };
--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -3,30 +3,75 @@ export const LANG_EN_NAME = 'en';
 export const LANG_EN_TRANS = {
     'delivery_place':'Delivery Place',
     // main menu
-    'home':'الرئيسية',
-    'offers':'العروض',
-    'my-orders': 'طلباتى',
-    'account': 'الحساب',
-    'my-account': 'حسابى',
-    'help': 'مساعدة',
-    'free_credit': 'رصيد مجانى',
-    'my_wallet': 'محفظتى',
+    'home':'Home',
+    'offers':'Offers',
+    'my-orders': 'My Orders',
+    'account': 'Account',
+    'my-account': 'My Account',
+    'help': 'Help',
+    'free_credit': 'Free Credit',
+    'my_wallet': 'My Wallet',
+    'my_points': 'My points',
     // categories
-    'all': 'الكل',
+    'all': 'ALL',
     // account menu
-    'profile': 'الصفحة الشخصية',
-    'sign_out': 'تسجيل خروج',
-    'sign_in': 'تسجيل دخول',
+    'profile': 'Profile',
+    'sign_out': 'Sign Out',
+    'sign_in': 'Sign In',
     // shopping cart
-    'shopping_cart': 'عربة التسوق',
-    'view_cart': 'عرض العربة',
-    'item': 'منتج',
-    'empty_cart': 'ليس لديك اى منتجات فى عربة التسوق',
+    'shopping_cart': 'Shopping Cart',
+    'view_cart': 'View Cart',
+    'item': 'Item',
+    'empty_cart': 'No Items in shopping cart',
     // footer
-    'copy_rights': 'جميع حقوق النشر محفوظه لسنه 2018 ©',
+    'copy_rights': 'All copyrights © reserved 2018',
     // language
     'en' : 'English',
     'ar' : 'عربى',
     // search
-    'search':'Type to search...'
-}
+    'search':'Type to search ...',
+    // order
+    'delivery_info': 'Delivery Info',
+    'products': 'Products',
+    'unavailable_products': 'Unavailable Products',
+    'payment_details': 'Payment Details',
+    'order_received': 'Order Received',
+    'order_received_msg': 'your order has been received and will be prepared in time',
+    'prepare_order': 'Prepare Order',
+    'prepare_order_msg': 'your order is being prepared',
+    'on_way': 'Order is on the way',
+    'on_way_msg': 'your order is on the way to you right now',
+    'order_delivered': 'Order Delivered',
+    'order_delivered_msg': 'order delivered, enjoy your day',
+    'delivery_time': 'Delivery Time',
+    'delivery_location': 'Delivery Time',
+    'order_value': 'Order value',
+    'delivery_fees': 'Delivery Fees',
+    'vat_value': 'Vat Value',
+    'vat_rate': 'Vat Rate',
+    'discount_value': 'Discount Value',
+    'total_price': 'Total Price',
+    'SAR': 'SAR',
+    // product
+    'category': 'Category',
+    'size': 'Size',
+    // my account
+    'my_account' : 'My Account',
+    'name' : 'Name',
+    'email' : 'Email',
+    // My Wallet
+    'wallet_balance' : 'Wallet balance',
+    'active' : 'Active',
+    'used' : 'Used',
+    'expired' : 'Expired',
+    // sign in
+    'country_key' : 'Country Key',
+    'mobile' : 'Mobile',
+    'full_name' : 'Full Name',
+    'social_sign_in' : 'or Sign in with one click',
+    'no_account' : 'Don\'t have an account? Sign up now!',
+    // sms dialog
+    'sms_confirm': 'Confirm Phone Number',
+    'code_number': 'Enter number you have received',
+    'code': 'Code'
+};
